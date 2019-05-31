f37fe880ea8b68e25b6459403f4716610299782b
Rick Giner <rickginer@gmail.com>
2019-05-31 10:46:30 +1000

    refactor

--- toDo.specs.js
+++ toDo.specs.js
@@ -1,27 +1,26 @@
 describe('Testing the functionality', ()=>{
 
-    it('should add an item', ()=>{
-        let todo = new ToDo();
-        let item = {
-            title: "get milk",
-            complete: false
-        }
-        const done = todo.addTodo(item)
-        expect(todo.getItems().length).toBe(1);
-    })
-
-    it('should delete an item', ()=>{
-        let todo = new ToDo();
-        let item = {
+    let todo, item, item2;
+    beforeEach(function(){
+        todo = new ToDo();
+        item = {
             id: 1,
             title: "get milk",
             complete: false
         }
-        let item2 = {
+        item2 = {
             id: 2,
             title: "get beer",
             complete: false
-        }
+        }   
+    })
+
+    it('should add an item', ()=>{
+        todo.addTodo(item)
+        expect(todo.getItems().length).toBe(1);
+    })
+
+    it('should delete an item', ()=>{
         todo.addTodo(item)
         todo.addTodo(item2)
         todo.delete(2)
@@ -29,17 +28,6 @@ describe('Testing the functionality', ()=>{
     })
 
     it('should mark item as complete', function(){
-        let todo = new ToDo();
-        let item = {
-            id: 1,
-            title: "get milk",
-            complete: false
-        }
-        let item2 = {
-            id: 2,
-            title: "get beer",
-            complete: false
-        }
         todo.addTodo(item)
         todo.addTodo(item2)
         todo.complete(2)
