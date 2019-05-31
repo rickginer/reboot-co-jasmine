03ab79c6bb53d40c20fa6ae8f9edd64ee3589682
Rick Giner <rickginer@gmail.com>
2019-05-31 10:28:35 +1000

    delete an item

--- toDo.js
+++ toDo.js
@@ -1,9 +1,16 @@
 function ToDo(){
+
     let todo = [];
+    
     this.addTodo = (item) => {
         todo.push( item)
     }
+    
     this.getItems = () => {
         return todo
+    }
+
+    this.delete = (id) => {
+        todo =  todo.filter(item => item.id !== id)
     }
 }
--- toDo.specs.js
+++ toDo.specs.js
@@ -11,7 +11,21 @@ describe('Testing the functionality', ()=>{
     })
 
     it('should delete an item', ()=>{
-    //...
+        let todo = new ToDo();
+        let item = {
+            id: 1,
+            title: "get milk",
+            complete: false
+        }
+        let item2 = {
+            id: 2,
+            title: "get beer",
+            complete: false
+        }
+        todo.addTodo(item)
+        todo.addTodo(item2)
+        todo.delete(2)
+        expect(todo.getItems()[0].id).toBe(1);
     })
 
     it('should mark item as complete', ()=>{
